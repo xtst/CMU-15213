e45454f310257506c1fe21b0055d69c36f9c61d8
xtst <xintongsanto@yeah.net>
2022-04-09 21:59:47 +0800

    file input done

--- CSAPP/cachelab-handout/csim.c
+++ CSAPP/cachelab-handout/csim.c
@@ -1,7 +1,39 @@
 #include "cachelab.h"
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#define debug(x)                                                                                                                           \
+	if (debugMod) { printf("%s\t=%d\n", (#x), x); }
+#define sdebug(x)                                                                                                                          \
+	if (debugMod) { printf("%s\t=%s\n", (#x), x); }
 
-int main()
-{
-    printSummary(0, 0, 0);
-    return 0;
+int debugMod;
+int s, E, b;
+int miss, hit, eviction;
+
+int main(int argc, char **argv) {
+	char command;
+	char fileName[20];
+	while ((command = getopt(argc, argv, "vs:E:b:t:")) != -1) {
+		if (command == 'v') {
+			debugMod = 1;
+			debug(debugMod);
+		} else if (command == 't') {
+			strcpy(fileName, optarg);
+			sdebug(fileName);
+		} else {
+			int value = atoi(optarg);
+			if (command == 's') s = value;
+			if (command == 'E') E = value;
+			if (command == 'b') b = value;
+			debug(s);
+			debug(E);
+			debug(b);
+		}
+	}
+
+	printSummary(hit, miss, eviction);
+	return 0;
 }
