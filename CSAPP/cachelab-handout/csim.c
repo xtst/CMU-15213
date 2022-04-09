101d6dc18e125c4145e54858e8a13500d67e31b3
xtst <xintongsanto@yeah.net>
2022-04-10 00:40:41 +0800

    debuging

--- CSAPP/cachelab-handout/csim.c
+++ CSAPP/cachelab-handout/csim.c
@@ -29,11 +29,11 @@ void change_to_bits(char *s, char *address) {
 
 void malloc_space(int s, int E, int b) {
 	int S = pow(2, s);
-	cache = malloc(S * sizeof(int *));
+	cache = malloc(S * sizeof(int **));
 	for (int i = 0; i < S; i++) {
 		cache[i] = malloc(E * sizeof(int *));
 		for (int j = 0; j < E; j++) {
-			cache[i][j] = malloc(pow(2, b) * sizeof(int) + mbit - s - b + 2);
+			cache[i][j] = malloc((pow(2, b) + mbit - s - b + 2) * sizeof(int));
 			cache[i][0] = 0;
 		}
 	}
@@ -63,42 +63,75 @@ void eviction_cache() {
 int get_b(int s, int E, int b, char *address) {
 	int res = 0;
 	for (int i = b - 1; i >= 0; i--) {
-		res >>= 1;
+		res <<= 1;
 		res |= address[i] - '0';
 	}
 	return res;
 }
 int get_s(int s, int E, int b, char *address) {
 	int res = 0;
-	for (int i = b + s - 1; i >= b; i--) {
-		res >>= 1;
-		res |= address[i] - '0';
+	for (int i = mbit - b - s + 1; i <= mbit - b; i++) {
+		res <<= 1;
+		res |= (address[i] - '0') & 1;
 	}
 	return res;
 }
 int get_t(int s, int E, int b, char *address) {
 	int res = 0;
 	for (int i = mbit - 1; i >= s + b; i++) {
-		res >>= 1;
+		res <<= 1;
 		res |= address[i] - '0';
 	}
 	return res;
 }
-void replace_cache(int s, int E, int b, int id, char *address) {}
+void replace_cache(int s, int E, int b, int id, char *address, int s_now) {
+	int t = mbit - s - b, min_id = 1e9;
+	int E_now = 0;
+	for (int i = 0; i < E; i++) {
+		if (cache[s_now][E][0] < min_id) {
+			min_id = cache[s_now][E][0];
+			E_now = i;
+		}
+	}
+	if (cache[s_now][E_now][0] != 0) { eviction_cache(); }
+	cache[s_now][E_now][0] = id;
+	for (int i = 1; i <= t; i++) { cache[s_now][E][i] = address[mbit - t] - '0'; }
+}
 
-void load_cache(int s, int E, int b, int id, char *address) {
+void load_cache(int s, int E, int b, int id, char *address, int go_replace) {
 	int t = mbit - s - b;
-	for (int i = 0; i < E; i++)
-		if (cache[s][E][0]) {
+	int s_now = get_s(s, E, b, address);
+	for (int i = 0; i < E; i++) {
+		if (cache[s_now][E][0]) {
+			int flag = 1;
 			for (int i = 1; i <= t; i++) {
-				// if(address[mbit-t] )
+				if (address[mbit - i] != cache[s_now][E][i] + '0') {
+					flag = 0;
+					break;
+				}
+			}
+			if (flag) {
+				hit_cache();
+				return;
 			}
 		}
+	}
+	miss_cache();
+	if (go_replace) replace_cache(s, E, b, id, address, s_now);
+	return;
 }
 
 void process(int s, int E, int b, int id, char *option, char *address) {
 	if (option[0] == 'I') return;
-	if (option[0] == 'L') {}
+	if (option[0] == 'L') { load_cache(s, E, b, id, address, 1); }
+	if (option[0] == 'M') {
+		load_cache(s, E, b, id, address, 1);
+		load_cache(s, E, b, id, address, 0);
+	}
+	if (option[0] == 'S') {
+		load_cache(s, E, b, id, address, 1);
+		// load_cache(s, E, b, id, address, 0);
+	}
 }
 
 int main(int argc, char **argv) {
