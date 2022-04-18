f809586a2a64325f915115740fad9818a6a141cd
xtst <xintongsanto@yeah.net>
2022-04-19 00:19:28 +0800

    new init

---
+++ CSAPP/lab/my-malloclab-data/mm.c
@@ -0,0 +1,98 @@
+/*
+ * mm-naive.c - The fastest, least memory-efficient malloc package.
+ *
+ * In this naive approach, a block is allocated by simply incrementing
+ * the brk pointer.  A block is pure payload. There are no headers or
+ * footers.  Blocks are never coalesced or reused. Realloc is
+ * implemented directly using mm_malloc and mm_free.
+ *
+ * NOTE TO STUDENTS: Replace this header comment with your own header
+ * comment that gives a high level description of your solution.
+ */
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "memlib.h"
+#include "mm.h"
+
+/*********************************************************
+ * NOTE TO STUDENTS: Before you do anything else, please
+ * provide your team information in the following struct.
+ ********************************************************/
+team_t team = {
+	/* Team name */
+	"santo",
+	/* First member's full name */
+	"santo",
+	/* First member's email address */
+	"santo@santo",
+	/* Second member's full name (leave blank if none) */
+	"",
+	/* Second member's email address (leave blank if none) */
+	""};
+
+/* single word (4) or double word (8) alignment */
+#define ALIGNMENT 8
+
+/* rounds up to the nearest multiple of ALIGNMENT */
+#define ALIGN(size) (((size) + (ALIGNMENT - 1)) & ~0x7)
+
+#define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
+
+/*
+ * mm_init - initialize the malloc package.
+ */
+int *list_node;
+int mm_init(void) {
+	// void *p = mem_heap_lo();
+	// list_node = mem_sbrk(sizeof(int) * 32);
+	// for (int i = 0; i < 32; i++) list_node[i] = 0;
+	// if (((int)p & 7) == 0) exit(1);
+	// void *p = mem_sbrk(sizeof(int) *);
+	return 0;
+}
+
+/*
+ * mm_malloc - Allocate a block by incrementing the brk pointer.
+ *     Always allocate a block whose size is a multiple of the alignment.
+ */
+int high_size(size_t x) {
+	int p = 1;
+	while (p < (int)x) { p <<= 1; }
+	return p;
+}
+void *mm_malloc(size_t size) {
+	int newsize = ALIGN(size + SIZE_T_SIZE);
+	void *p = mem_sbrk(newsize + 1);
+	if (p == (void *)-1)
+		return NULL;
+	else {
+		*(size_t *)p = size;
+		return (void *)((char *)p + SIZE_T_SIZE);
+	}
+}
+
+/*
+ * mm_free - Freeing a block does nothing.
+ */
+void mm_free(void *ptr) {}
+
+/*
+ * mm_realloc - Implemented simply in terms of mm_malloc and mm_free
+ */
+void *mm_realloc(void *ptr, size_t size) {
+	void *oldptr = ptr;
+	void *newptr;
+	size_t copySize;
+
+	newptr = mm_malloc(size);
+	if (newptr == NULL) return NULL;
+	copySize = *(size_t *)((char *)oldptr - SIZE_T_SIZE);
+	if (size < copySize) copySize = size;
+	memcpy(newptr, oldptr, copySize);
+	mm_free(oldptr);
+	return newptr;
+}
