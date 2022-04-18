961eaa5bae660aadc3e4555ebef5218f7a99a8e8
xtst <xintongsanto@yeah.net>
2022-04-19 00:27:35 +0800

    size_t fault corrected

--- CSAPP/lab/my-malloclab-data/mm.c
+++ CSAPP/lab/my-malloclab-data/mm.c
@@ -42,27 +42,35 @@ team_t team = {
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
-int high_size(size_t x) {
-	int p = 1;
+size_t high_size(size_t x) {
+	size_t p = 1;
 	while (p < (int)x) { p <<= 1; }
 	return p;
 }
 /*
  * mm_init - initialize the malloc package.
  */
-int mm_init(void) { return 0; }
+int *list_node;
+int mm_init(void) {
+	// void *p = mem_heap_lo();
+	// list_node = mem_sbrk(sizeof(int) * 32);
+	// for (int i = 0; i < 32; i++) list_node[i] = 0;
+	// if (((int)p & 7) == 0) exit(1);
+	// void *p = mem_sbrk(sizeof(int) *);
+	return 0;
+}
 
 /*
  * mm_malloc - Allocate a block by incrementing the brk pointer.
  *     Always allocate a block whose size is a multiple of the alignment.
  */
 void *mm_malloc(size_t size) {
-	int newsize = ALIGN(size + SIZE_T_SIZE);
+	size_t newsize = high_size(ALIGN(size + SIZE_T_SIZE));
 	void *p = mem_sbrk(newsize);
 	if (p == (void *)-1)
 		return NULL;
 	else {
-		*(size_t *)p = size;
+		*(size_t *)p = newsize;
 		return (void *)((char *)p + SIZE_T_SIZE);
 	}
 }
