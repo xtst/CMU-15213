0ab4f5eec409706116b7e62536bde12025a98e4e
xtst <xintongsanto@yeah.net>
2022-04-27 23:16:30 +0800

    update book

--- CSAPP/lab/my-proxylab-data/proxy.c
+++ CSAPP/lab/my-proxylab-data/proxy.c
@@ -1,14 +1,27 @@
+#include "csapp.h"
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
 /* Recommended max cache and object sizes */
 #define MAX_CACHE_SIZE 1049000
 #define MAX_OBJECT_SIZE 102400
-
+#define MAX_STRING_SIZE 12345
 /* You won't lose style points for including this long line in your code */
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
-
+static const char *error = "Error!";
 int main(int argc, char **argv) {
-	int t = atoi(argv[1]);
+	char s[MAX_STRING_SIZE];
+	int t = 0;
+	if (argc == 2)
+		t = atoi(argv[1]);
+	else {
+		t = 555;
+		// printf("%s", error);
+		// return -1;
+	}
+	// printf("%d\n", t);
 	printf("%s", user_agent_hdr);
+	while (getline(s, MAX_STRING_SIZE, stdin)) { printf("%s", s); }
+	// rio
 	return 0;
 }
