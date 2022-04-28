a6272afe3668d8c617ac28db5900da00a1991c8b
xtst <xintongsanto@yeah.net>
2022-04-28 10:18:04 +0800

    update

---
+++ CSAPP/lab/my-proxylab-data/2.c
@@ -0,0 +1,9 @@
+/* $begin tinymain */
+/*
+ * tiny.c - A simple, iterative HTTP/1.0 Web server that uses the
+ *     GET method to serve static and dynamic content.
+ *
+ * Updated 11/2019 droh
+ *   - Fixed sprintf() aliasing issue in serve_static(), and clienterror().
+ */
+#include "csapp.h"
